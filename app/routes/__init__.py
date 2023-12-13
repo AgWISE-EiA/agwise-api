9eb09f022ab2763fed3fd19cd9e5cc339b2df982
Sammy Barasa <barsamms@gmail.com>
2023-12-13 11:45:41 +0300

    feat: added feature to allow generation of open api documentation

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,15 +1,52 @@
 # routes/route.py
-from flask import Blueprint, request, jsonify
+import json
+from http import HTTPStatus
+
+from flask import Blueprint, request, jsonify, make_response
+from flask_openapi3 import Tag, Info
+from flask_openapi3 import APIBlueprint, OpenAPI
+from pydantic import BaseModel, Field
 
 from app.core import limiter
+from app.my_logger import MyLogger
 from app.potato.agwise_potato import AgWisePotato
+from app.routes.FertilizerData import FertilizerQuery, FertilizerResponse
 
-api_v1 = Blueprint('api_v1', __name__)
+tag = Tag(name='AgWise', description="AgWise API")
+security = [{"jwt": []}]
+
+logging = MyLogger()
+
+
+class Unauthorized(BaseModel):
+    code: int = Field(-1, description="Status Code")
+    message: str = Field("Unauthorized!", description="Exception Information")
+
+
+api_v1 = APIBlueprint(
+    'api_v1',
+    __name__,
+    url_prefix='/api/v1',
+    # abp_tags=[tag],
+    # abp_security=security,
+    # abp_responses={"401": Unauthorized},
+    # disable openapi UI
+    doc_ui=True
+)
+
+# api_v1 = Blueprint('api_v1', __name__)
+
+potato_fr_tag = Tag(name="potato", description="Fertilizer data for potato in Rwanda")
 
 
 # limiter.limit("1/minute")(api_v1)
 
-@api_v1.route('/fr-potato', methods=['POST'])
+# @api_v1.route('/fr-potato', methods=['POST'])
+@api_v1.get("/fr-potato",
+            summary="Fetch rwanda potato FR",
+            doc_ui=False,
+            tags=[potato_fr_tag],
+            deprecated=True)
 @limiter.limit("30/minute")
 def list_all_data():
     data: object = request.get_json()
@@ -22,10 +59,16 @@ def list_all_data():
     return jsonify(result)
 
 
-@api_v1.route('/fertilizer-data', methods=['GET'])
+@api_v1.get("/fertilizer-data",
+            summary="Get fertilizer recommendations data in Rwanda",
+            tags=[potato_fr_tag],
+            responses={200: FertilizerResponse},
+            )
 @limiter.limit("30/minute")
-def fetch_fertilizer_data():
+def fetch_fertilizer_data(query: FertilizerQuery):
     lat, lon = None, None
+
+    logging.info(query)
 
     coordinates = request.args.get('coordinates')
     region = request.args.get('region')
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, jsonify
+from flask_openapi3 import Info, Tag, OpenAPI, Server
+from pydantic import BaseModel
 
 from app.core import limiter
 from app.routes import api_v1
 
-app = Flask(__name__)
+info = Info(title="AgWise API", version="1.0.0")
 
-app.register_blueprint(api_v1, url_prefix='/api/v1')
+servers = [
+    Server(url="http://127.0.0.1:5000"),
+    Server(url="https://akilimo.org:5000"),
+]
+
+# app = Flask(__name__)
+app = OpenAPI(__name__,
+              info=info,
+              servers=servers)
+
+# app.register_blueprint(api_v1, url_prefix='/api/v1')
+app.register_api(api_v1)
 
 app.config['RATELIMIT_HEADERS_ENABLED'] = True
 app.json.sort_keys = False
